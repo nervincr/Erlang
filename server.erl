d3ad43760ed998fc49a98c1356de6015b8d9e122
nervincr <nervincr@users.noreply.github.com>
2019-10-28 10:18:52 -0600

    Se adjunta documentación.

--- server.erl
+++ server.erl
@@ -2,7 +2,7 @@
 -compile(export_all).
 -import(lists, [reverse/1,fac/1, fib/1]).
 
-ini() ->
+iniciar_server() ->
     {ok, Listen} = gen_tcp:listen(2345,
                                 [binary, {packet, 4},
                                 {reuseaddr, true},
@@ -17,17 +17,19 @@ loop(Socket) ->
     receive
         {tcp, Socket, Bin} -> 
             io:format("<Server> Binarios recibidos = ~p~n", [Bin]),
-            {Func, Val} = binary_to_term(Bin),
-            Respuesta = resultado(Func,Val),
+            %Val = binary_to_term(Bin),
+            %Respuesta = funcion:fac(Val),
+            {Opc, Val} = binary_to_term(Bin),            
+            Respuesta = operación(Opc,Val),
             gen_tcp:send(Socket, term_to_binary(Respuesta)),
             loop(Socket);
         {tcp_close, Socket} ->
             io:format("<Server> Socket cerrado~n")
     end.
 
-resultado(Param, Val) when Param == 'fac' ->
+operación(Opc, Val) when Opc == 'fact' ->
     funcion:fac(Val);
-resultado(Param, Val) when Param == 'fib' ->
+operación(Opc, Val) when Opc == 'fibo' ->
     funcion:fib(Val);
-resultado(_, _) ->
-    io:format("<Server> Default~n").
+operación(_, _) ->
+    io:format("<Server> Opción Inválida!~n").
